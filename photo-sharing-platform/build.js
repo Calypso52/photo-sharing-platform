56dcaa089cbd86af3691e4891757d157f9a2f609
zyt <243080140@qq.com>
2022-07-05 15:17:37 -0400

    add path

--- photo-sharing-platform/build.js
+++ photo-sharing-platform/build.js
@@ -1,6 +1,6 @@
 const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/build.js');
 const config = defaults.__get__('config');
-
+const path = require('path');
 
 config.resolve.alias['@'] = path.resolve('src');
